4c730c0b8cd91e7bd5f8118c8839732676b7c4df
JUNE\june <kuc2477@gmail.com>
2016-10-26 18:00:11 +0900

    change parsing logic to adapt lines as well

--- quiz/parser.py
+++ quiz/parser.py
@@ -2,7 +2,7 @@
 import itertools as it
 import re
 import os.path
-from . import utils
+from .utils import noteng, notengline, iseng, isengline, ispureline
 
 
 class Parser(object):
@@ -17,7 +17,7 @@ class Parser(object):
     def parse(self, path):
         target_content = self._parse_text(self._read_text(path))
         target_items = self._parse_content(target_content)
-        return {k: v for k, v in self._normalize(target_items)}
+        return {k: v for k, v in self._normalize_items(target_items)}
 
     def parse_title(self, path):
         text = self._read_text(path)
@@ -37,25 +37,80 @@ class Parser(object):
     def _parse_text(self, text):
         content_regex = self.CONTENT_REGEX_FORMAT.format(self._target)
         content_regex = re.compile(content_regex)
-        title, content = content_regex.findall(text)[0]
-        return content
+        try:
+            title, content = content_regex.findall(text)[0]
+            return content
+        except IndexError:
+            return ''
 
     def _parse_content(self, content):
-        # first normalize unicode
-        content = content.replace('\xa0', '')
+        # normalize content and split into lines
+        content = self._normalize_content(content)
+        lines = content.splitlines()
 
-        # start reducing process
+        # store indicies of lines that has been parsed 
+        parsed = set()
+        # store parsed english-translation tuples
         items = []
-        while content:
-            # take english and reduce content
-            english = ''.join(it.takewhile(lambda c: utils.iseng(c), content)).strip()
-            content = ''.join(it.dropwhile(lambda c: utils.iseng(c), content)).strip()
-            # take translation and reduce content
-            translation = ''.join(it.takewhile(lambda c: utils.noteng(c), content)).strip()
-            content = ''.join(it.dropwhile(lambda c: utils.noteng(c), content)).strip()
-            # append items to the list
-            items.append((english, translation))
+
+        for i, line in enumerate(lines):
+            # jump the line if it has been parsed already
+            if i in parsed:
+                continue
+            # start line reducing process if given line is mixed up with 
+            # english and non-english
+            elif not ispureline(line):
+                while line:
+                    # take english and reduce content
+                    english = ''\
+                        .join(it.takewhile(lambda c: iseng(c), line))\
+                        .strip()
+                    line = ''\
+                        .join(it.dropwhile(lambda c: iseng(c), line))\
+                        .strip()
+
+                    print('============================')
+                    print('english: {}'.format(english))
+
+                    # take translation and reduce content
+                    translation = ''\
+                        .join(it.takewhile(lambda c: noteng(c), line))\
+                        .strip()
+                    line = ''\
+                        .join(it.dropwhile(lambda c: noteng(c), line))\
+                        .strip()
+
+                    print('============================')
+                    print('korean: {}'.format(translation))
+
+                    # append items to the list and mark the line as parsed
+                    items.append((english, translation))
+                    parsed.add(i)
+            # otherwise when the line is pure english or pure non-english
+            else:
+                reduced = it.dropwhile(lambda l: isengline(l), lines[i:])
+                english_lines = it.takewhile(lambda l: isengline(l), lines[i:])
+                english = '\n'.join(english_lines).strip()
+
+                reduced = it.dropwhile(lambda l: notengline(l), reduced)
+                translation_lines = it.takewhile(lambda l: notengline(l), reduced)
+                translation = '\n'.join(translation_lines).strip()
+
+                indicies = list(range(
+                    i, i + len(english_lines) + len(translation_lines)
+                ))
+
+                # append items to the list and mark lines as parsed
+                items.append((english, translation))
+                parsed.union(indicies)
+
         return items
 
-    def _normalize(self, items):
+    def _normalize_content(self, content):
+        norm_regex = re.compile(r'\n+')
+        content = norm_regex.sub('\n', content)
+        content = content.replace('\xa0', '')
+        return content.strip()
+
+    def _normalize_items(self, items):
         return [(k, v) for k, v in items if v]
--- quiz/utils.py
+++ quiz/utils.py
@@ -5,7 +5,7 @@ import re
 
 
 def noteng(c):
-    return c in ['—', ',', '[', ']', '(', ')'] or \
+    return c in ['—', ',', '[', ']', '(', ')', '\n', "'", '"'] or \
         c not in [*string.ascii_letters,]
 
 
@@ -16,6 +16,24 @@ def iseng(c):
         *string.punctuation.replace('~', ''),
         '—'
     ]
+
+
+def notengline(l):
+    words = l.split(sep=' ')
+    try:
+        return all([noteng(c) for c in words[0]]) and \
+            all([noteng(c) for c in words[len(words) - 1]])
+    except IndexError:
+        return True
+
+
+def isengline(l):
+    words = l.split(sep=' ')
+    try:
+        return all([iseng(c) for c in words[0]]) and \
+            all([noteng(c) for c in words[len(words) - 1]])
+    except IndexError:
+        return True
 
 
 def available_notes(root='./'):
