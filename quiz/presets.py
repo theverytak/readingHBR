c7ef801a5e1d9a3f3028c25659ea188679b5f572
JUNE\june <kuc2477@gmail.com>
2016-10-28 12:45:00 +0900

    better comment format

--- quiz/presets.py
+++ quiz/presets.py
@@ -1,10 +1,8 @@
 # Belief thresholds for the heuristic logic which determines whether a line is 
-# pure or not. 
-#
-# For example, we will consider a line is pure when it's purity is larger then 
-# 0.7 within '문장' target context. However, we will consider any line that 
-# has lower than perfect purity(1) as an impure line within '단어' target 
-# context.
+# pure or not. For example, we will consider a line is pure when it's purity 
+# is larger then 0.7 within '문장' target context. However, we will consider 
+# any line that has lower than perfect purity(1) as an impure line within 
+# '단어' target context.
 PURITY_HEURISTIC_PROBS= {
     '단어': 1,
     '문장': 0.7,
